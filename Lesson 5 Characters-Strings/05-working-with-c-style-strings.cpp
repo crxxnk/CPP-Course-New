537bdd8a53360df5a3ffe121348ac8d96e0410d5
crxxnk <135547044+crxxnk@users.noreply.github.com>
2024-07-21 17:23:41 +0300

    diakopes

--- Lesson 5 Characters-Strings/05-working-with-c-style-strings.cpp
+++ Lesson 5 Characters-Strings/05-working-with-c-style-strings.cpp
@@ -19,8 +19,28 @@ int main() {
   cin >> last_name;
   cout << "--------------------------------------" << endl;
 
-  cout << "Hello, your first name has " << strlen(first_name) << " characters" << endl;
-  cout << "and your last name has " << strlen(last_name) << " characters" << endl;
+  //! Instead of returning an int, the strlen function returns a size_t value which is an unsigned value we don't know(unsigned long, unsigned long long)
+  cout << "Hello " << first_name << " your first name has " << strlen(first_name) << " characters" << endl;
+  cout << "and your last name " << last_name << " has " strlen(last_name) << " characters" << endl;
+
+  strcpy(full_name, first_name); // Copies the first_name to the full_name variable
+  strcat(full_name, " "); // Adds a space to the full_name variable
+  strcat(full_name, last_name); // Adds the last_name to the full_name variable
+
+  cout << "Your full name is " << full_name << endl;
+
+  /*
+  ! If we actually enter our full name, the compiler will stop the output whenever it sees a whitespace so it would only display the first name
+  cout << "Enter your full name: ";
+  cin >> full_name;
+  cout << "Your full name is " << full_name << endl; 
+  */
+
+  //? Instead, we do this:
+
+  cout << "Enter your full name: ";
+  cin.getline(full_name, 50); // Max characters are 50. It stops when it reaches 50 chars or when the user presses enter
+  cout << "Your full name is " << full_name << endl;
 
   return 0;
 }
