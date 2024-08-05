1ae2c03fe39a0989c3df49204c31a21237cda50b
crxxnk <135547044+crxxnk@users.noreply.github.com>
2024-08-05 17:54:33 +0300

    pointer pitfalls

--- Lesson 7 Pointers-References/07-pointers.cpp
+++ Lesson 7 Pointers-References/07-pointers.cpp
@@ -328,6 +328,31 @@ int main() {
 
   / *score_ptr = 86; //! ERROR
   score_ptr = &low_score; //! ERROR
+
+  ! Potential Pointer Pitfalls
+
+  * Uninitialized pointers:
+  int *int_ptr; // pointing anywhere
+  / *int_ptr = 100; // potential crash
+
+  * Dangling pointers:
+  ? Pointer that is pointing to released memory
+  * For example, 2 pointers point to the same data
+  * 1 pointer releases the data with delete
+  * The other pointer accesses the release data
+  ? Pointer that points to memory that is invalid
+  * We saw this when we returned a pointer to a function local variable
+
+  * Not checking if new failed to allocate memory:
+  * If new fails an exception is thrown
+  * We can use exception handling to catch exceptions
+  * Dereferencing a null pointer will cause your program to crash
+
+  * Leaking memory:
+  * Forgetting to release allocated memory with delete
+  ! If you lose your pointer to the storage allocated on the heap, you have no way to get to that storage again
+  * The memory is orphaned or leaked
+  * One of the most common pointer problems
   */
 
   return 0;
